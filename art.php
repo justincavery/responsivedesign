d71a1d5ba41153f38994db450d70ba3c64a3afe6
Justin Avery <justincavery@gmail.com>
2014-01-22 16:57:46 +0000

    Print Styles applied, article page layout initial changes

---
+++ art.php
@@ -0,0 +1,40 @@
+<?php include 'parts/doctype.php';?>
+<body class="articles">
+	<div class="header">
+		<?php include 'parts/header.php';?>
+		<?php include 'parts/nav.php';?>
+		<a class="tablet toggle-subnav" href="#"><span class="nav-bar clearfix"><span aria-hidden="true" data-icon="&#57349;" ></span>Subnav</span></a>
+		<ul class="breadcrumbs">
+			<li><a href="/"><span aria-hidden="true" data-icon="&#8962;" ></span></a></li>
+			<li><a href="articles.php">Articles</a></li>
+			<li>Interview with Dan Mall</li>
+		</ul>
+	</div>
+	<div class="content">
+		<article class="article" role="main">
+			<header>
+      <h1>Interview with Daniel Mall</h1>
+         <p class="lead">Dan Mall takes some time away from his new studio at Super Friendly to talk us through his own responsive design process and shares a few of his experiences with his own projects.
+          Secondary navigation in responsive design</p>
+  </header>
+<aside class="article-metadata">
+	<h1>Details</h1>
+<ul>
+  <li><span>Author:</span> <a href="#">Justin Avery</a></li>
+  <li><span>Published:</span> 14/01/2014</li>
+  <li><span>Tags:</span><a href="#">Strategy</a>, <a href="#">Interview</a>, <a href="#">Opinion</a></li>
+ <li><span>Cateogry:</span> Interview</li>
+</ul>
+
+		</aside>
+
+      <p><em><strong>Just incase a few of our readers haven't come across your name before perhaps you could tell us a little about how you got started, your history and how you ended up where you are today?</strong></em></p><p><strong>Daniel:</strong> Of course! Ever since I was a kid, I knew I wanted to do something artistic. I used to draw all the time, so I figured I would go to school to be a 3D animator. Once I went to school, I quickly realised that I wasn't very good at 3D animation, but I did very much enjoy combining graphic design, interaction design, and motion—a perfect fit for what the web was in those days. The rest is history.</p><p>(For those curious, I tell the full story in <a href="http://thegreatdiscontent.com/dan-mall">my interview</a> with The Great Discontent.)</p><p><em><strong>You have <a href="http://superfriend.ly/Reading-Is-Fundamental">RIF</a> as one of your clients and you are also writing an on going story about how your are <a href="http://danielmall.com/articles/rif-kickoff">approaching the project</a> (thank you for that by the way… amazing). There seems to be a lot of back and forth communication with the client throughout the project process and tonnes of iterations. While I know that *is* what is needed for a successful responsive project I've also found that it is biggest concern and the question that I am asked the most by agencies – "How are you able to successfully price the RWD process without either quoting stupid amounts of money or taking a loss on the project?"</strong></em></p><p><strong>Daniel:</strong> There's a lot that goes into pricing projects. Most people and agencies that I've come across charge for their time, so naturally it's a risk to do something new where you can't quantify your time as definitively. That's not exclusive to responsive web design; the same time happened in our industry when designers and developers were moving from table-based layouts to CSS-based layouts.</p><p>I've found that my projects tend more to consist of things I don't know how to do than things I do know how to do. (Shh! Don't tell my clients!) Rather than billing by time, I try to charge by value for the entire project. Part of that value pricing means leaving enough margin to experiment, which means time to do work and re-do work without any penalty to my client's business or my own business.</p><p>I talk a lot about these kinds of things on <a href="http://businessologyshow.biz/wordpress">The Businessology Show</a>, a podcast about the business of design and the design of business.</p><p><em><strong>Moodpik was another client you've got who went with a desktop and mobile design, but not all the way to responsive. With web app sites like <a href="http://weatherrr.net">weatherrr.net</a> and <a href="http://forecast.io">forecast.io</a> now making some traction do you think there's an argument for responsive web apps?</strong></em></p><p><strong>Daniel:</strong> I think there's always an argument to be made for building flexible solutions, whether responsive or otherwise. For me, responsive isn't a goal; it's a strategy we can use to accomplish our goals. It's a way to future-proof.</p><p><em><strong>I love the design work that you personally produce! Can you explain your workflow when approaching a redesign and how, if at all, you have had to change that approach when you are designing responsively?</strong></em></p><p><strong>Daniel:</strong> Thanks! Yes, my process has changed almost entirely. Before responsive web design came along, I think I took for granted that clients understood the process of a redesign. I could send over a picture of a website and they'd get what it meant with minimal explanation.</p><p>Because responsive web design is so relatively new, I find myself over-communicating with clients, not in a patronizing way but spending more time in explaining a concept that may be foreign to them. Not surprisingly, the projects where I'm doing the most education are the most smooth. As a result, I'm actually being more and more intentional about sending over "deliverables" that can't easily be understood without an accompanying conversation. I'm almost forcing myself to have to communicate, and I think that's a good thing.</p><p><em><strong>So that's the end of the "Dan" specific questions. The last two questions are the same for everyone.</strong></em></p><p><em><strong>What is your favourite implementation of responsive design?</strong></em></p><p><strong>Daniel:</strong> I love NY Magazine's <a href="http://nymag.com/thecut">The Cut</a> and the <a href="http://www.jackdaniels.com">Jack Daniel's</a> site because they're great reminders that we don't have to sacrifice art direction to create well-crafted responsive experiences.</p><p><em><strong>If someone is beginning their new responsive site next week what is one piece of advice you would give them?</strong></em></p><p><strong>Daniel:</strong> Prototype, prototype, prototype. I learn so many things through bouncing back and forth quickly between Photoshop and Sublime Text. I'd recommend ditching a waterfall process with testing smaller concepts. For example, let's say you have an idea for how navigation could work across multiple screen sizes. Before going any further, code that small piece to see if your assumptions are correct. I promise you'll discover small surprises that you hadn't anticipated when you actually see it in action.</p><p><em><strong>Thanks again for being a part of the series Dan! How can people hire you or find out more information?</strong></em></p><p><strong>Daniel:</strong> You can see my professional adventures on the <a href="http://superfriend.ly">SuperFriendly site</a>, check out my personal ramblings at <a href="http://danielmall.com">danielmall.com</a>, or argue with me on Twitter at <a href="http://twitter.com/danielmall">@danielmall</a>. Cheers!</p>
+
+</article>
+<?php include 'parts/related-articles.php'; ?>
+</div>
+		<?php include 'parts/footer.php'; ?>
+
+	<?php include 'parts/javascript.php'; ?>
+</body>
+</html>
